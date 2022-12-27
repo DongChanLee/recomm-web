71823dab5d84faaec4e161b344ae3a2e9c3cc0e9
dc <evanlee0407@gmail.com>
2022-12-27 17:23:54 +0900

    start React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import { Button, Navbar, Container, Nav } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  let message = '리액트 프론트엔드 개발중';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark" className="main-nav">
+        <Container>
+          <Navbar.Brand href="#home" className="nav-title">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+      <h4 style={ {color : 'blue', fontSize : '16px'} }>{ message }</h4>
+      
     </div>
+
+   
+
   );
 }
 
